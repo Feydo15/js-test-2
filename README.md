51e790995881c747ea536a56a6050c4bc8876d7e
Feydo15 <feydollor@gmail.com>
2022-06-09 14:38:17 +0200

    jst

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-test-2
+
+this is a short js test that has 4 katas or questions that was 2 hours long.
--- index.js
+++ index.js
@@ -1,45 +1,52 @@
-
 // question 1
 
-let list = [{name:"Thabo" , mark : 40} , {name:"smith" , mark : 33} , {name:"Nean" , mark : 22}];
+let list = [
+  { name: "Thabo", mark: 40 },
+  { name: "smith", mark: 33 },
+  { name: "Nean", mark: 22 },
+];
 let total = 60;
 
- const getAverage = (list, total) =>{
- list.map(list =>  `${list.mark}`.reduce((a, b) => a + b, 0) / total * 100 )
- }
- getAverage()
+const getAverage = (list, total) => {
+  list.map((list) => (`${list.mark}`.reduce((a, b) => a + b, 0) / total) * 100);
+};
+getAverage();
 
 // question 2
- const editName =(list)=>{
- 
- let Names = list.map(function(list) {
-     return list.name;
-    });
-let edit = Names.splice(2,1,"john")
+const editName = (list) => {
+  let Names = list.map(function (list) {
+    return list.name;
+  });
+  let edit = Names.splice(2, 1, "john");
 
-    console.log(edit)
-}
+  console.log(edit);
+};
 
-editName(list)
+editName(list);
 
 // question 3
 
-const str = '4T354B3434234234234'
+const str = "4T354B3434234234234";
 
-const newStr = str.replace(/\D/g, '').split(" , ").map(x=>+x).reduce((a, b) => a + b, 0);
-console.log(newStr)
+const newStr =(str)=>{
+    str.replace(/\D/g, "")
+  .split(" , ")
+  .map((x) => +x)
+  .reduce((a, b) => a + b, 0);
 
-//question 4 
-
-let arr = ['5' , 'g' , 3 , 5 , 10 , 'dfdfdf']
-
-const removeNuN =(arr)=>{
-
-    for(i in arr){
-
-        arr.replace(/\D/g, '');
-       }
 }
+newStr(str)
+console.log(newStr);
 
-removeNuN(arr)
-console.log(removeNuN(arr))
+//question 4
+
+let arr = ["5", "g", 3, 5, 10, "dfdfdf"];
+
+const removeNuN = (arr) => {
+  for (i in arr) {
+    return arr.replace(/\D/g, "");
+  }
+};
+
+removeNuN(arr);
+console.log(removeNuN(arr));
